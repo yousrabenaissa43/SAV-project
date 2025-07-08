40815afeba054922e2f4dde227bf4d8f854c27be
yousrabenaissa43 <yousrabenaissa43@gmail.com>
2025-07-08 19:54:28 +0200

    first commit

---
+++ afrique-med/src/test/java/afriqueMed/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package afriqueMed;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
